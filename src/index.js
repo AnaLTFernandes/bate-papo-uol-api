6bcc06a5f57013987e07050c92d4ad0b104195d4
Ana Luiza Teixeira Fernandes <analuizateixeirafernandes184@gmail.com>
2022-08-31 15:00:12 -0300

    feat: post a participant in the path "/participants" without mongodb

--- src/index.js
+++ src/index.js
@@ -6,4 +6,27 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const participants = [];
+const messages = [];
+
+server.post('/participants', (req, res) => {
+    const { name } = req.body;
+    const hasName = participants.find(participant => name === participant.name);
+
+    if (!name) {
+        res.sendStatus(422);
+        return;
+    }
+
+    if (hasName) {
+        res.status(409).send({ message:'Nome já existente!' });
+        return;
+    }
+
+    participants.push({ name, lastStatus: Date.now() });
+    messages.push({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: new Date().toLocaleTimeString('pt-br') });
+    
+    res.status(201).send({ message:'Usuário criado', messages });
+});
+
 server.listen(5000, () => console.log('Servidor rodando na porta 5000'));
